75dc9e790f8a9f785d543c72f3b4d39ca8557cc4
Carina Rey <carinareyv@gmail.com>
2024-05-29 13:15:52 -0400

    first commit

---
+++ 125.validPalindrome.js
@@ -0,0 +1,30 @@
+var isPalindrome = function (s) {
+
+
+    var u = s.replace(/[^0-9a-zA-Z]/g, '').toLowerCase();
+
+
+    var start = 0;
+    var end = u.length - 1
+    if (u.length < 2) {
+        return true;
+    }
+
+    while (start < end) {
+
+        if (u[start] == u[end]) {
+
+            start++;
+            end--;
+        } else {
+            return false;
+        }
+
+
+
+    }
+
+
+    return true;
+};
+
