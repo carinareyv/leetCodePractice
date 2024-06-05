1f337ba61f0eb9eab1d897ff65ca2902459be284
Carina Rey <carinareyv@gmail.com>
2024-06-04 23:34:21 -0400

    easy-hashmap

---
+++ HashMaps/Easy/219.containsDuplicateII.js
@@ -0,0 +1,19 @@
+var containsNearbyDuplicate = function (nums, k) {
+    var hashMap = new Map();
+    var res = false;
+
+
+    for (var i = 0; i < nums.length; i++) {
+        var distinct = nums[i]
+        if (hashMap.has(distinct)) {
+            if(Math.abs(hashMap.get(distinct) - i) <= k) {
+                res = true
+            } else {
+                hashMap.set(distinct, i)
+            }
+        } else {
+            hashMap.set(distinct, i)
+        }
+    }
+    return res;
+};
