7ea48b0f8087eaf51bda5395abd5609aa565cd12
Carina Rey <carinareyv@gmail.com>
2024-06-07 16:36:27 -0400

    easy-hashmap

---
+++ HashMaps/Easy/202.happyNumber.js
@@ -0,0 +1,21 @@
+var isHappy = function (n) {
+
+var hashHappy = new Map();
+while (n != 1) {
+    var process = 0;
+    var current = n;
+    while (current != 0) {
+        process += (current % 10) ** 2
+        current = Math.floor(current / 10);
+    }
+    if(hashHappy[process]) {
+        return false;
+    }
+   
+     hashHappy[process] = true
+     n = process
+    
+}
+
+return true
+};
